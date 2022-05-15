c9cab050dfb1307e5668f023410c798b05cad10b
GT-DOM\clambhope <cody@grouptree.com>
2022-05-15 05:56:31 +0100

    Add view password functionality

--- PasswordValidation/Content/Scripts/UIFunctions.js
+++ PasswordValidation/Content/Scripts/UIFunctions.js
@@ -1,4 +1,8 @@
-﻿var UIFunctions = {
+﻿$(document).on('click', '[data-show-password]', function () {
+    UIFunctions.Forms.ShowPassword($(this));
+})
+
+var UIFunctions = {
     Forms: {
         ValidatePassword: function ($form, inVersion) {
             var formData = $form.serialize();
@@ -11,6 +15,20 @@
                     $('#validation-results').html(data);
                 }
             });
+        },
+        ShowPassword: function ($this) {
+            var $input = $('[name="' + $this.data('show-password') + '"]');
+
+
+            if ($input.attr('type') == "password") {
+                $input.attr('type', 'text');
+            } else {
+                $input.attr('type', 'password');
+            }
+
+
+            $this.toggleClass('fa-eye');
+            $this.toggleClass('fa-eye-slash');
         }
     }
 }
